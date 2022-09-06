690d7816793d5b89732901acb855ba03d505690e
4rnav <arnavsarkar10@gmail.com>
2022-09-06 12:19:58 +0530

    setup MongoDB connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const App = () => {
-    return(
-        <div>
+    return (
+        <div class="root">
             <h1>
                 Hello World
             </h1>
